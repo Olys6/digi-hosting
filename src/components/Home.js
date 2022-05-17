5b5d71a4c559d7f453bd7000f3d57a3cd6dc96d8
olys6 <oliver.skjevesland@gmail.com>
2022-05-17 18:50:35 +0200

    almost completed one half of the features list and started the second list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, ListGroup } from 'react-bootstrap'
+import { Card, ListGroup, Button } from 'react-bootstrap'
 import Plans from './HomeComponents/Plans.js'
 
 const Home = () => {
@@ -17,8 +17,8 @@ const Home = () => {
             <Card.Title style={{ backgroundColor: "white", color: "#080494", marginTop: "20px", padding: "10px", boxSizing: "border-box", fontSize: "45px", margin: "-15px 0 0 20px" }}>
               Versatile, powerful web hosting
             </Card.Title>
-            </Card.Body>
-          </div>
+          </Card.Body>
+        </div>
 
         {/* <div id="rightHomeHero">
 
@@ -30,11 +30,11 @@ const Home = () => {
         <div className="whyChooseUsCircles">
           <div class="whyChooseUsCircleAndDesc" data-aos="fade-up" data-aos-duration="400">
             <div className="whyChooseUsCircle"><i class="fas fa-hdd fa-3x"></i></div>
-          
-          <p style={{ fontSize: "25px", fontWeight: "bold", color: "#080494" }}>100% SSD Hosting</p>
-          <p style={{ fontSize: "20px", color: "#080494", fontWeight: "500" }}>
-            Solid state drive storage means faster loading websites.
-          </p>
+
+            <p style={{ fontSize: "25px", fontWeight: "bold", color: "#080494" }}>100% SSD Hosting</p>
+            <p style={{ fontSize: "20px", color: "#080494", fontWeight: "500" }}>
+              Solid state drive storage means faster loading websites.
+            </p>
           </div>
           <div class="whyChooseUsCircleAndDesc" data-aos="fade-up" data-aos-duration="700">
             <div className="whyChooseUsCircle"><i class="fas fa-user-tie fa-3x"></i></div>
@@ -53,7 +53,7 @@ const Home = () => {
             </p>
           </div>
           <div class="whyChooseUsCircleAndDesc" data-aos="fade-up" data-aos-duration="1300">
-            <div className="whyChooseUsCircle"><i className="fa-solid fa-ticket" style={{ fontSize: "65px"}}></i></div>
+            <div className="whyChooseUsCircle"><i className="fa-solid fa-ticket" style={{ fontSize: "65px" }}></i></div>
 
             <p style={{ fontSize: "25px", fontWeight: "bold", color: "#080494" }}>Reliable Support</p>
             <p style={{ fontSize: "20px", color: "#080494", fontWeight: "500" }}>
@@ -66,15 +66,32 @@ const Home = () => {
         <h2>Features</h2>
         <div className="featuresLists">
           <div className='featuresList'>
-            <div style={{ fontWeight:"bold", fontSize: "30px", backgroundColor: "#e4304c", height: "4rem", color: "white", display: "flex", alignItems: "center", paddingLeft: "15px", boxSizing: "border-box" }}>
+            <div style={{ fontWeight: "bold", fontSize: "30px", backgroundColor: "#e4304c", height: "4rem", color: "white", display: "flex", alignItems: "center", paddingLeft: "15px", boxSizing: "border-box" }}>
               Hosting Feature list
             </div>
             <div className='featuresListItem'>
-
+              <p><i className="fas fa-check"></i> Website Builder</p> <p>Unlimited Subdomains</p> <Button variant='danger' style={{ backgroundColor: "#e4304c", border: "0" }}>More</Button>
+            </div>
+            <div className='featuresListItem'>
+              <p><i className="fas fa-check"></i> Unlimited FTP/SFTP Access</p> <p>Additional FTP/SFTP Accounts</p> <Button variant='danger' style={{ backgroundColor: "#e4304c", border: "0" }}>More</Button>
+            </div>
+            <div className='featuresListItem'>
+              <p><i className="fas fa-check"></i> Online File Manager</p> <p>Scheduled Tasks</p> <Button variant='danger' style={{ backgroundColor: "#e4304c", border: "0" }}>More</Button>
+            </div>
+            <div className='featuresListItem'>
+              <p><i className="fas fa-check"></i> Directory Indexing</p> <p>Access/Error/Bandwidth Logs</p> <Button variant='danger' style={{ backgroundColor: "#e4304c", border: "0" }}>More</Button>
+            </div>
+            <div className='featuresListItem'>
+              <p><i className="fas fa-check"></i> Mapped Domains</p> <p>Edge Caching for Speed</p> <Button variant='danger' style={{ backgroundColor: "#e4304c", border: "0" }}>More</Button>
             </div>
           </div>
           <div className='featuresList'>
-
+            <div style={{ fontWeight: "bold", fontSize: "30px", backgroundColor: "#e4304c", height: "4rem", color: "white", display: "flex", alignItems: "center", paddingLeft: "15px", boxSizing: "border-box" }}>
+              Hosting Feature list
+            </div>
+            <div className='featuresListItem'>
+              <p><i className="fas fa-check"></i> Autoscaling Resources</p> <p>Google-Based DNS Servers</p> <Button variant='danger' style={{ backgroundColor: "#e4304c", border: "0" }}>More</Button>
+            </div>
           </div>
         </div>
       </section>
